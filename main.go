8a21896b042c924e24acd365948137c3fadbd519
Narendra Annabathula <narendraannabathula@VIRA.attlocal.net>
2025-03-21 04:36:24 -0700

    WIP

--- main.go
+++ main.go
@@ -25,28 +25,11 @@ func getLinesChannel(f io.ReadCloser) <-chan string {
 	retChan := make(chan string)
 	go func() {
 		defer f.Close()
-		currentLine := ""
+		
 		for {
-			data := make([]byte, 8)
-			_, err := f.Read(data)
-			if err == io.EOF {
-				if currentLine != "" {
-					retChan <- currentLine
-					//fmt.Printf("read: %s\n", currentLine)
-
-				}
-				close(retChan)
-				break
+			
 
 			}
-			currentLine = currentLine + string(data)
-			parts := strings.Split(currentLine, "\n")
-			if len(parts) == 2 {
-				//fmt.Printf("read: %s\n", parts[0])
-				retChan <- parts[0]
-				currentLine = "" + parts[1]
-			}
-
 		}
 	}()
 	return retChan
